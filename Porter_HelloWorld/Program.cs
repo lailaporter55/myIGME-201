de950a785b67df1f1b9799cb79f3cf0baede0c87
lailaporter55 <lmp3003@rit.edu>
2023-09-01 12:39:55 -0400

    PE1 - HelloWorld Porter

---
+++ Porter_HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porter_HelloWorld
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5; 
+            int num2 = 10;
+            //adds two integer numbers and prints the sum 
+            Console.WriteLine("num1 + num2 = ", num1 + num2);
+            //multiplys two integer numbers and prints the product 
+            Console.WriteLine("num1 * num2 = ",num1 * num2);
+            
+            
+        }
+    }
+}
