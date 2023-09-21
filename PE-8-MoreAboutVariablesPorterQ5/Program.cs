fd3d2702b77051fc494a9b99bf46298f5fab2e69
lailaporter55 <lmp3003@rit.edu>
2023-09-20 21:53:48 -0400

    PE8 #7

--- PE-8-MoreAboutVariablesPorterQ5/Program.cs
+++ PE-8-MoreAboutVariablesPorterQ5/Program.cs
@@ -11,18 +11,24 @@ namespace PE_8_MoreAboutVariablesPorterQ5
         static void Main(string[] args)
         {
             double[,,] dTripArray = new double[5,5,5];
-            //double y = 0.0;
-            //double x = 0.0;
-            //double z = (3.0 * (Math.Pow(y, 2.0) + (2.0 * x) - 1.0);
-            for(double x = 0; x>= -1 && x <= 1; x+= 0.1)
+            while (true)
             {
-                Console.WriteLine(x);
-                for(double y = 0; y >= 1 && y<= 4; y += 0.1)
+                for(int i = 0; i < dTripArray.GetLength(0); i++)
                 {
-                    Console.WriteLine(y);
-                    double z = (3.0 * (Math.Pow(y, 2) + (x * 2.0) - 1.0);
-                    double z = z;
+                    for (double x = 0; x >= -1 && x <= 1; x += 0.1)
+                    {
+                        Console.WriteLine(x);
+                        for (double y = 0; y >= 1 && y <= 4; y += 0.1)
+                        {
+                            Console.WriteLine(y);
+                            double z = (3.0 * (Math.Pow(y, 2) + (x * 2.0) - 1.0);
+                            dTripArray.SetValue(x, i);
+                            dTripArray.SetValue(y, i);
+                            dTripArray.SetValue(z, i);
+                        }
+                    }
                 }
+                
             }
         }
     }
---
+++ PE8-MoreABTVarPorterQ7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_MoreABTVarPorterQ7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
