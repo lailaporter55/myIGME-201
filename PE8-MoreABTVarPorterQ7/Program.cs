f57aa64e45a1d3a1b219af38c361137e89a37bdc
lailaporter55 <lmp3003@rit.edu>
2023-09-24 23:00:14 -0400

    pe9

--- PE8-MoreABTVarPorterQ7/Program.cs
+++ PE8-MoreABTVarPorterQ7/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PE8_MoreABTVarPorterQ7
@@ -9,7 +10,15 @@ namespace PE8_MoreABTVarPorterQ7
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            char[] reversed;
+
+            Console.WriteLine("Enter a word");
+            string sWord = Console.ReadLine();
+            char[] wordChar = sWord.ToCharArray();
+             reversed = char[].Reverse(wordChar);
+            Console.WriteLine("reversed = " reversed);
+
         }
     }
 }
---
+++ PE9-Q2Porter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Q2Porter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
