f115b3f00fde138d2a047b3d4cb2a919e708c312
lailaporter55 <lmp3003@rit.edu>
2023-10-10 18:48:36 -0400

    vehicle2

--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
-    public class Class1
+    public abstract class Vehicle
     {
+        public virtual void LoadPassenger()
+        {
+
+        }
     }
+    public abstract class Car: Vehicle
+    {
+
+    }
+    public abstract class Train: Vehicle { }
+    public interface IPassangerCarrier
+    {
+        void LoadPassanger();
+    }
+    public interface IHeavyLoadCarrier { }
+    public class Compact : Car, IPassangerCarrier
+    {
+        public void LoadPassanger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class SUV : Car, IPassangerCarrier
+    {
+        public void LoadPassanger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Pickup : Car, IPassangerCarrier, IHeavyLoadCarrier
+    {
+        public void LoadPassanger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class PassengerTrain : Train, IPassangerCarrier
+    {
+        public void LoadPassanger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public class __424DoubleBogey : Train, IHeavyLoadCarrier { }
 }
