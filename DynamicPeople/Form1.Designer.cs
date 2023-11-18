c2006672b4ca67dc50c9c2af44b4082e78e03764
lailaporter55 <lmp3003@rit.edu>
2023-11-17 21:06:55 -0500

    courses

---
+++ DynamicPeople/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DynamicPeople
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            splitContainer1 = new SplitContainer();
+            teacherButton = new Button();
+            studentButton = new Button();
+            imageList1 = new ImageList(components);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(studentButton);
+            splitContainer1.Panel1.Controls.Add(teacherButton);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 132;
+            splitContainer1.TabIndex = 0;
+            // 
+            // teacherButton
+            // 
+            teacherButton.Location = new Point(8, 8);
+            teacherButton.Name = "teacherButton";
+            teacherButton.Size = new Size(112, 95);
+            teacherButton.TabIndex = 0;
+            teacherButton.Text = "button1";
+            teacherButton.UseVisualStyleBackColor = true;
+            // 
+            // studentButton
+            // 
+            studentButton.Location = new Point(8, 123);
+            studentButton.Name = "studentButton";
+            studentButton.Size = new Size(112, 100);
+            studentButton.TabIndex = 1;
+            studentButton.Text = "button2";
+            studentButton.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "student.png");
+            imageList1.Images.SetKeyName(1, "teacher.png");
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Button studentButton;
+        private Button teacherButton;
+        private ImageList imageList1;
+    }
+}
--- Friend/Program.cs
+++ Friend/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Friend
 {
+    public class Friend
+    {
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+    }
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+            friend.name = "Jules";
+            friend.greeting = "Hey bestie Jules";
+            friend.birthdate = DateTime.Parse("2002-12-30");
+            friend.address = "AXID house, Rochester, NY, 14623"; 
+
+            enemy = friend;
+            enemy.greeting = "Boo Jules";
+            enemy.address = "Return to sender. Address Unknown";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
         }
     }
 }
