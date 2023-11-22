34b722286bfa35238ff6953123627cbfb44c5c81
lailaporter55 <lmp3003@rit.edu>
2023-11-21 22:49:39 -0500

    tut 6

---
+++ Dyscord/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dyscord
+{
+    public partial class SettingsForm : Form
+    {
+        public int myPort; 
+        public SettingsForm(Form owner, int nPort)
+        {
+            InitializeComponent();
+            this.Owner = owner;
+            this.CenterToParent(); 
+            this.myPort = nPort;
+            this.portTextBox.Text = nPort.ToString();
+            this.startButton.Click += new EventHandler(StartButton__CLick); 
+            this.portTextBox.KeyPress += new EventHandler(PortTextBox__KeyPress)
+        }
+        public void StartButton__CLick(object sender, EventArgs e)
+        {
+            this.myPort = Int32.Parse(this.portTextBox.Text);
+            this.Close();
+        }
+        public void PortTextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(Char.IsDigit(e.KeyChar) || e.KeyChar == '\b')
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Dyscord/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO; 
+
+namespace Dyscord
+{
+    public partial class DyscordForm : Form
+    {
+        string targetUser;
+        string TargetIp;
+        int targetPort;
+        string myIp; 
+        int myPort;
+        Thread thread;
+        Socket listener; 
+        public DyscordForm()
+        {
+            InitializeComponent();
+            this.Show();
+            SettingsForm settingsForm = new SettingsForm(this, myPort); 
+            settingsForm.ShowDialog();
+        }
+    }
+}
